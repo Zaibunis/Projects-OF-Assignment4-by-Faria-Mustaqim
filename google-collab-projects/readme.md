e3d84471d8ddaeae4a0dabd075d8a2e6240dba79
Zaibunis <zainabmustaqeem123@hotmail.com>
2025-03-24 21:22:29 +0500

    first commit

---
+++ google-collab-projects/readme.md
@@ -0,0 +1,12 @@
+📌 Google Colab Projects:
+Below are the required Google Colab projects. Click on the links to access them:
+
+🚀 Compulsory Projects:
+
+[Count-Down-Timer-Project](https://colab.research.google.com/drive/1T8gTaDuYjDw98lztzFghFBoHyju7mnwN?usp=sharing)
+
+[Mad-Libs-Project](https://colab.research.google.com/drive/1D6vB351jkUsCdsp9AodzfSzsWV_P-I0M?usp=sharing)
+
+[Hang-Man-Project](https://colab.research.google.com/drive/1ohHENOUxjREULRHgSMnvr8irj2NZeUIf?usp=sharing)
+
+[Rock-Paper-Scissors-Project](https://colab.research.google.com/drive/1eXx6DGFxujd8X_mn3Na4Eti5NqP9mmPd?usp=sharing)
---
+++ homeworks-projects/app.py
@@ -0,0 +1,32 @@
+#add two numbers
+
+def main():
+    print("This program adds two numbers.")
+    num1 : str = input("Enter first number: ")
+    num1 : int = int(num1)
+    num2  : str = input("Enter second number: ")
+    num2 : int = int(num2)
+    total : int = num1 + num2
+    print("The total is " + str(total) + ".")
+
+if __name__ == '__main__':
+    main()
+
+#agreement-bot
+
+def main():
+    print("Favorite Animal Matcher")
+    animal1 : str = input("What's your favorite animal? ")
+    animal2 : str = input(f"My favorite animal is also \033[1m{animal1}\033[0m!")
+    animal2 : str = str(animal2)
+
+if __name__ == '__main__':
+    main()
+
+#fahrenheit_to_celsius
+
+def main():
+    print()
+
+if __name__ == '__main__':
+    main()
